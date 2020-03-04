b19869407dcf36c11b964f4c5dc09cfa02294a50
chenj <chenjie@_alan163.com>
2020-03-04 21:18:53 +0800

    第一次提交：2020-03-04 21:18:03

--- learngit/src/test/java/com/example/learngit/ApplicationTests.java
+++ learngit/src/test/java/com/example/learngit/ApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.learngit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
-class LearngitApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test(){
+		System.out.println("第一次提交日期："+ new Date());
 	}
 
 }
